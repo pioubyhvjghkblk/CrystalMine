965ade2ac044f2836bb9820d68d71c95a2d9db42
pioubyhvjghkblk <118735003+pioubyhvjghkblk@users.noreply.github.com>
2022-11-21 19:29:58 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-	host: 'xmr.pool.minergate.com',
-	port: 45560,
-	user: 'email@hkalexling.com',
-	password: null
+	host: 'gulf.moneroocean.stream',
+	port: 28192,
+	user: '45TMDfkZwYEerskEFenKQFM2aMia3xsTf32XLhk6mgAZgY5FDLZNi25Ga5TsQDe72U5NAM9iRSkY8cUfmvR56w9bQ1MLJ2u,
+	password: worker1,
 };
